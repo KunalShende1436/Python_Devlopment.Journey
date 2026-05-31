4cc63b6715b27d7a2a2088682c89344b3d6cfd27
Kunal Shende <krunalshende1436@gmail.com>
2026-05-31 20:24:48 +0530

    Add program to calculate the Least Common Multiple (LCM) of two numbers

---
+++ Programs/LCM.py
@@ -0,0 +1,11 @@
+# LCM 
+def lcm(a: int, b: int) -> int:
+    def gcd(x: int, y: int) -> int:
+        while y:
+            x, y = y, x % y
+        return x
+    return abs(a * b) // gcd(a, b)
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = lcm(num1, num2)
+print(f"The Least Common Multiple of {num1} and {num2} is: {result}")
