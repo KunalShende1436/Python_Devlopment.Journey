64d3b3a600c84d16d5e1ac640963893d62cde73a
Kunal Shende <krunalshende1436@gmail.com>
2026-06-11 15:23:45 +0530

    Add program to perform basic arithmetic operations on two numbers

---
+++ Programs/number_operations.py
@@ -0,0 +1,23 @@
+num1 = input("Enter Num1=")
+num2 = input("Enter Nume2=")
+
+# Sum
+print(f"Sum of {num1} and {num2} is {int(num1) + int(num2)}")
+
+# Subtraction
+print(f"Subtraction of {num1}and {num2}is {int(num1)-int(num2)}")
+
+# Multiplication
+print(f"Multiplication of {num1} and {num2}is {int(num1)*int(num2)}")
+
+# Division
+print(f"Division of {num1}and {num2}is {int(num1)/int(num2)}")
+
+# Floor Division
+print(f"Floor Division of {num1}and {num2}is {int(num1)//int(num2)}")
+
+# Modulus
+print(f"Modulus of {num1}and {num2}is {int(num1)%int(num2)}")
+
+# Exponentiation
+print(f"Exponentiation of {num1}and {num2}is {int(num1)**int(num2)}")
